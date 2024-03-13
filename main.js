8a4e33a13a4320232bc3f3e5ea0a2311b870bad6
Eduardo Felipe <eduardofelipe_2000@hotmail.com>
2024-03-13 15:59:56 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,10 @@
+AOS.init();
+
+function sendwhatsapp(){
+    var phonenumber = '+5547999315082'
+
+    var name = document.querySelector('.name').value;
+
+    var url = 'https://wa.me' + phonenumber + '?text='
+    +'*Name :*' +name+"%0a" 
+}
